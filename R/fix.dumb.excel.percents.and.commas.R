16e9b40a2664d356789fb7b3d7f6c274f4edbcd8
David Whiting <david@davidwhiting.me.uk>
2012-07-19 20:32:12 +0100

    Fixed conversion bug; added option to specify columns to process

--- R/fix.dumb.excel.percents.and.commas.R
+++ R/fix.dumb.excel.percents.and.commas.R
@@ -1,26 +1,25 @@
 fix.dumb.excel.percents.and.commas <-
-function(x) {
+  function(x, cols = 1:ncol(x)) {
     ## When Excel saves columns of numbers formatted as percents
     ## as csv files it stupidly saves % symbols in csv files.
     ## This function strips the % and makes the number numeric.
-
-    ## x: can be either a dataframe or a vector
+    
     if (is.null(dim(x))) {
-        x <- gsub("%", "", x)
-		x <- gsub(",", "", x)
-        x <- as.numeric(x)
+      x <- gsub("%", "", x)
+      x <- gsub(",", "", x)
+      x <- as.numeric(x)
     } else {
-        for (i in 1:ncol(x)) {
-            if (is.factor(x[, i])) {
-                if (any(grepl("%", levels(x[, i])))) {
-                    x[, i] <- gsub("%", "", levels(x[, i]))
-                }
-				if (any(grepl(",", levels(x[, i])))) {
-					x[, i] <- gsub(",", "", levels(x[, i]))
-				}
-#				x[, i] <- as.numeric(as.character(x[, i]))
-            }
+      for (i in cols) {
+        if (is.factor(x[, i])) {
+          if (any(grepl("%", levels(x[, i])))) {
+            x[, i] <- gsub("%", "", levels(x[, i]))
+          }
+          if (any(grepl(",", levels(x[, i])))) {
+            x[, i] <- gsub(",", "", levels(x[, i]))
+          }
+          x[, i] <- as.numeric(as.character(x[, i]))
         }
+      }
     }
     x
-}
+  }
