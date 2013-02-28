421c1b55c7e813b8f7cdc47047271d595f72e3d9
David Whiting <dwhiting@nhs.net>
2013-02-28 16:34:33 +0000

    Added documentation for penultimate()

--- R/penultimate.R
+++ R/penultimate.R
@@ -1,3 +1,11 @@
+##' Return the penulimate item from a vector
+##' 
+##' @param x A vector
+##' @return The penultimate item from the vector. The
+##' data type is determined by the data type of the vector.
+##' @keywords utils
+##' 
+
 penultimate <- function(x){
   y <- sort(unique(x))
   if (length(y) > 1) {
