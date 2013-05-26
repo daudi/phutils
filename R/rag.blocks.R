085637617a75bd091a33fcaad93f01f225536264
David Whiting <david@davidwhiting.me.uk>
2013-05-26 20:44:26 +0100

    Added ability to produce rag-rated symbols

--- R/rag.blocks.R
+++ R/rag.blocks.R
@@ -1,4 +1,4 @@
-#' Add a block of rag-rated rectangles
+#' Add a block of rag-rated rectangles, or symbols.
 #' 
 #' The blocks are coloured according to the rag-rating and have the letter 
 #' of the rag-rating (helpful when printing in black and white). 
@@ -8,17 +8,24 @@
 #' 
 #' @param status A vector of "R", "A", and "G"s.
 #' @param cex Character expansion factor for the letters in the blocks.
+#' @param blocks If TRUE (the default) draw rectangular blocks. If FALSE, use the pch symbol(s)
+#' @param pch The symbol to use if blocks = FALSE. 
+#' @param symbol.cex Character expansion for the symbol. This is going to have to be large to be useful. The default is 20. This will need to be changed depending on the symbol (pch) used.
+#' @details When using symbols (blocks = FALSE) pch 15 to 20 are good, others also.
 #' @author David Whiting, david.whiting@@publichealth.me.uk
 #' @keywords utils
+#' @return Invisibly returns a list with the x co-ordinates of the rectangles and text.
 #' @export
 #' @examples
 #' 
 #' x <- c("R", "G", "A", "G")
 #' rag.blocks(x)
+#' rag.blocks(x, block = FALSE)
+#' 
 #' 
 
 
-rag.blocks <- function(status, cex = 1) {
+rag.blocks <- function(status, cex = 1, blocks = TRUE, pch = 16, symbol.cex = 20) {
   num.items <- length(status)
   rag <- c("R", "A", "G")
   rag.colours <- c("red", "orange", "green")
@@ -28,6 +35,11 @@ rag.blocks <- function(status, cex = 1) {
   rag.xpos2 <- seq(to = 1, by = 1 / num.items, length.out = num.items)
   rag.text.xpos <- (rag.xpos + rag.xpos2) / 2
   plot.new()
-  rect(rag.xpos, 0, 1, 1, col = rag.status)
+  if (blocks) {
+    rect(rag.xpos, 0, 1, 1, col = rag.status)
+  } else {
+    points(rag.text.xpos, rep(0.5, length(rag.text.xpos)), pch = pch, cex = symbol.cex, col = rag.status)
+  }
   text(rag.text.xpos, 0.5, labels = status, cex = cex)
+  invisible(list(rag.xpos = rag.xpos, rag.text.xpos = rag.text.xpos))
 }
