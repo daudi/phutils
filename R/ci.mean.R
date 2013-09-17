4b6f0fba8a9265e88e7ac997b6711d9e290964a8
David Whiting (XT2) <david.whiting@publichealth.me.uk>
2013-09-17 17:23:00 +0100

    Fixed ci.mean() help page.
    It had some % symbols which were interpreted as comments and therefore wiped out some of the details.

--- R/ci.mean.R
+++ R/ci.mean.R
@@ -4,9 +4,16 @@
 ##' Mean CI = mean +/- critical value * standard deviation/square root of n observations 
 ##' 
 ##' @param x A numeric vector
-##' @param p The percentage of the normal curve that you want to include. Conventionally 95% is often used (i.e. 0.95) and this is the default. Other values may be more appropriate, depending on the analysis.
+##' @param p The percentage of the normal curve that you want to include. 
+##' Conventionally 95\% is often used (i.e. 0.95) and this is the default. Other values 
+##' may be more appropriate, depending on the analysis.
 ##' @details 
-##' 90% and 95% confidence intervals are used most often as they strike a balance between confidence and accuracy. For example, a 99.999% confidence interval sounds like it should be the best, but in order to get a confidence that high, we need to have a very wide interval. To say that we are 99.999% confident that the mean is in the interval (-100, 100) is not useful, but to say that we are 90% confident that it is in (- 5, 5) is much better.
+##' 90\% and 95\% confidence intervals are used most often as they strike a balance 
+##' between confidence and accuracy. For example, a 99.999\% confidence interval sounds 
+##' like it should be the best, but in order to get a confidence that high, we need 
+##' to have a very wide interval. To say that we are 99.999\% confident that the mean 
+##' is in the interval (-100, 100) is not useful, but to say that we are 90\% confident 
+##' that it is in (- 5, 5) is much better.
 ##' 
 ##' @return A named vector with three elements: lower CI, mean, upper CI.
 ##' @export
