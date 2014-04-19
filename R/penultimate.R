7f356fe0f104f0d9c59aaa704bfe40e624c2c8e1
David Whiting <david@davidwhiting.me.uk>
2014-04-19 09:52:48 +0100

    Clarified return values of penultimate() and added examples.

--- R/penultimate.R
+++ R/penultimate.R
@@ -1,11 +1,19 @@
 ##' Return the penulimate item from a vector
 ##' 
 ##' @param x A vector
+##' @details The vector is sorted so this function returns the penultimate value, not just the second to last item in the list. See the examples.
 ##' @return The penultimate item from the vector. The
-##' data type is determined by the data type of the vector.
+##' data type is determined by the data type of the vector. This is the penultimate item by value, not by position in the original vector.
 ##' @keywords utils
 ##' 
 ##' @export
+##' @examples
+##' x <- 1:10
+##' x
+##' penultimate(x)
+##' x <- 10:1
+##' x
+##' penultimate(x)
 
 penultimate <- function(x){
   y <- sort(unique(x))
