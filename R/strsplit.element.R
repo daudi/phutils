caef3df3cedd0b59cf9077902215384c419ba90e
David Whiting <david@davidwhiting.me.uk>
2014-05-08 17:01:19 +0100

    Adding strsplit.element()
    Return a given element (by position) from a split string
    
    strsplit() splits a string and returns a list. This functions wraps around strsplit() to make it easy to extract the resulting list elements by position.

---
+++ R/strsplit.element.R
@@ -0,0 +1,19 @@
+##' Return a given element (by position) from a split string
+##' 
+##' @description strsplit() splits a string and returns a list. This functions wraps around
+##' strsplit() to make it easy to extract the resulting list elements by position.
+##' 
+##' @param x The string (vector) to be split
+##' @param split The string to use to split
+##' @param element A number specifying the position of the element within each result
+##' in the list to return from each split.
+
+
+strsplit.element <- function (x, split, element, ...) {
+  xx <- strsplit(x, split)
+  ret <- list()
+  for (i in 1:length(xx)) {
+    ret[[i]] <- xx[[i]][element]
+  }
+  ret
+}
