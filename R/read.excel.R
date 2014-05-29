51be9e9d5a0eb3789a3ac587f4a9ee281908568f
David Whiting <david.whiting@publichealth.me.uk>
2014-05-29 13:52:32 +0100

    Enhanced read.excl()
    1) Added cols.to.keep to allow the user to specify which columns from which sheet should be kept.
    
    2) Changed fix.cols to a list so it can be specified separately for each sheet.

--- R/read.excel.R
+++ R/read.excel.R
@@ -4,10 +4,12 @@
 ##' 
 ##' @param file The path to an excel file
 ##' @template keep.sheets
-##' @param fix.cols The columns to run through \code{\link{fix.dumb.excel.percents.and.commas}()}
+##' @param fix.cols A list of columns to run through \code{\link{fix.dumb.excel.percents.and.commas}()}
 ##' @param rbind If TRUE use \code{do.call("rbind", x)} to combine the sheets. See details. Defaults to FALSE.
 ##' @param save.sheet.name If TRUE the name of the sheet will be added to the data.frame as a new column 
+##' @param cols.to.keep A list of vectors that specifies (by number) which columns to keep from each sheet
 ##' @param ... Other parameters to be passed to \code{\link{read.csv}()}
+##' 
 ##' 
 ##' @details This only works on Windows. By default it reads in all sheets from an excel file. You can use 
 ##' keep.sheets to specify the name of specific sheets to read. See \code{\link{excelToCsv()}} for more details about how this works.
@@ -16,7 +18,17 @@
 ##' work as expected if all of the sheets
 ##' have exactly the same structure. 
 ##' 
-##' @return If keep.sheets contains the name of more than one sheet 
+##' If \code{cols.to.keep} and \code{fix.cols} are specified, the numbering of the columns to fix using \code{fix.cols} 
+##' applies before \code{cols.to.keep} is considered (i.e. before columns are dropped). \code{cols.to.keep} still reads in 
+##' all columns initially, and then drops those that are not needed. As with standard R indexing, a negative number can 
+##' be used to specify columns to drop.
+##' 
+##' Please note that \code{keep.sheets} drops sheets early in the function, before \code{cols.to.keep} 
+##' and \code{fix.cols} are used, so the lists for \code{cols.to.keep} and \code{fix.cols} do not need to consider
+##' these sheets.
+##' 
+##' 
+##' @return If \code{keep.sheets} contains the name of more than one sheet 
 ##' the function returns a list of dataframes. If there is only one sheet 
 ##' it is returned as a data.frame.
 ##' 
@@ -24,18 +36,26 @@
 ##' @seealso \code{\link{excelToCsv}}, \code{\link{read.csv}},
 ##' \code{\link{fix.dumb.excel.percents.and.commas}}
 
-read.excel <- function(file, keep.sheets = NULL, fix.cols = NULL, rbind = FALSE, save.sheet.name = FALSE, ...) {
+read.excel <- function(file, keep.sheets = NULL, fix.cols = NULL, rbind = FALSE, save.sheet.name = FALSE, 
+                       cols.to.keep = NULL, ...) {
   tmp <- tempfile()
   dir.create(tmp)
   excelToCsv(file, keep.sheets = keep.sheets, target.dir = tmp)
   x <- file.info(list.files(path = tmp, pattern = ".*csv$"))
   csv.files <- rownames(x)
+  if (!is.null(cols.to.keep))
+    stopifnot(length(csv.files) == length(cols.to.keep))
+    
   x <- list()
+  i <- 0
   for (thisfile in csv.files) {
+    i <- i + 1
     sheet.name <- gsub("(.*).csv$", "\\1", thisfile)
-    y <- read.csv(file.path(tmp, thisfile), ...)  
+    y <- read.csv(file.path(tmp, thisfile), ...)
     if (!is.null(fix.cols))
-      y <- fix.dumb.excel.percents.and.commas(y, cols = fix.cols)
+      y <- fix.dumb.excel.percents.and.commas(y, cols = fix.cols[[i]])
+    if (!is.null(cols.to.keep))
+      y <- y[, cols.to.keep[[i]]]
     
     if (save.sheet.name)
       y$sheet.name <- sheet.name
