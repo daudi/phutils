67b5ec01a904c8dedf3a37ccd1ecd7638eaf2d12
David Whiting <david@davidwhiting.me.uk>
2014-06-09 19:58:36 +0100

    Added option to return the last match in split string.

--- R/strsplit.element.R
+++ R/strsplit.element.R
@@ -3,19 +3,27 @@
 ##' @description strsplit() splits a string and returns a list. This functions wraps around
 ##' strsplit() to make it easy to extract the resulting list elements by position.
 ##' 
+##' 
 ##' @param x The string (vector) to be split
 ##' @param split The string to use to split
 ##' @param element A number specifying the position of the element within each result
-##' in the list to return from each split.
+##' in the list to return from each split. Or the word "last" in which case the last element is 
+##' returned regardless of the length of each split.
 ##' 
 ##' @export
 
 
+
 strsplit.element <- function (x, split, element, ...) {
   xx <- strsplit(x, split)
+  if (!is.numeric(element)) {
+    element <- unlist(lapply(xx, length))
+  } else {
+    element <- rep(element, length(xx))
+  }
   ret <- list()
   for (i in 1:length(xx)) {
-    ret[[i]] <- xx[[i]][element]
+    ret[[i]] <- xx[[i]][element[i]]
   }
   ret
 }
