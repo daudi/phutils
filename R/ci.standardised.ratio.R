fa0a98dc8a26cb3388ea1c43db4cd29249399a98
David Whiting <david@davidwhiting.me.uk>
2014-06-20 18:38:58 +0100

    Fixing ci.poisson() code regression and adding ci.standardised.ratio()
    In an earlier clean-up of the code I mixed up calculating poission confidence intervals with calculating confidence intervals around a ratio. This commit should fix that and add a new function for calculating CIs around a ratio.

---
+++ R/ci.standardised.ratio.R
@@ -0,0 +1,40 @@
+##' Function to calculate confidence intervals around a standardised ratio
+##' 
+##' This function is useful for calculating confidence intervals for a
+##' standardised mortality ratio (SMR) e.g. HSMR.
+##' 
+##' @param observed The number of observed events.
+##' @param time 
+##' @param p The percentile of the normal distribution (i.e. 0.95 = 95\% CIs)
+##' @param method exact or anscombe
+##' 
+
+##' @return The lower and upper number of events.
+##' @author Terry Therneau, Mayo Clinic
+##' @references Therneau, T.
+##' https://stat.ethz.ch/pipermail/r-help/2008-February/154897.html
+##' @keywords utils
+##' @seealso 
+##' \code{\link{ci.mean}}, 
+##' \code{\link{ci.proportion}}, 
+##' \code{\link{ci.poisson}}, 
+##' \code{\link{ci.standardised.rate}}
+##' @examples
+##' 
+##' ### Example from Statistics with Confidence (1989), page 60.
+##' 
+##' observed <- 64
+##' expected <- 45.6
+##' ci.standardised.ratio(observed, expected)
+##' 
+##' 
+##' 
+##' @export
+"ci.standardised.ratio" <- 
+  function(observed, expected, time = 1, p = 0.95, method = c("exact", "anscombe") ) {
+    res <- ci.poisson(observed, time, p, method)
+    res <- c(lower.CI = res['lower.CI'] / expected, SR = res['count'] / expected, upper.CI = res['upper.CI'] / expected)
+        
+    res
+  }
+
