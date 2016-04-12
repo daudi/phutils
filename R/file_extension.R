916e8845c29fd5b1db00d0c1c66b04da1d0b81d4
David Whiting <david@davidwhiting.me.uk>
2016-04-12 19:52:43 +0100

    Added file_extension()

---
+++ R/file_extension.R
@@ -0,0 +1,17 @@
+##' Return the extension of a file name
+##' 
+##' 
+##' This is a simple utility function that returns the extension from a file name.
+##' 
+##' @param x A file name (a character string)
+##' @return A character string
+##' @export
+##' @examples 
+##' 
+##' file.extension("temp.txt")
+
+file_extension <- function(x) {
+  x <- unlist(strsplit(x, "\\."))
+  x[length(x)]
+}
+
