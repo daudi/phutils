a98334f7642e8cb0f992e2d0b67a815a60803a04
David Whiting <david@davidwhiting.me.uk>
2017-05-28 21:15:24 +0100

    Added man-roxygen template

---
+++ man-roxygen/keep.sheets.R
@@ -0,0 +1,4 @@
+##' @param keep.sheets A vector of sheet names to keep. This only works on windows (but presumably 
+##' libreoffice code could be added). All sheets are converted, and if keep.sheets is NULL (the default) 
+##' then all sheets will be returned. If keep.sheets 
+##' is not NULL then only those named in keep.sheets are kept.
