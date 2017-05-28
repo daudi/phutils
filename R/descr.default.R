2e62399ee33e2d177e67b54d578e35207cdece55
David Whiting <david@davidwhiting.me.uk>
2017-05-28 11:01:28 +0100

    Removed descr* functions as they are not ready yet

--- R/descr.default.R
+++
@@ -1,9 +0,0 @@
-##' describe a vector
-##' 
-##' @param x A  vector
-##' @param colname The name of the vector (optional)
-##' @export
-##' 
-descr.default <- function(...) {
-  descr.character(...)
-}
--- R/descr.integer.R
+++
@@ -1,9 +0,0 @@
-##' describe an integer vector
-##' 
-##' @param x An integer vector
-##' @param colname The name of the vector (optional)
-##' @export
-##' 
-descr.integer <- function(x, this_col = NULL) {
-  descr.numeric(x, this_col)
-}
